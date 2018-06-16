d1652647765bddbf58fc0bf382ac6d20734944c3
jazmin.ferreiro <jazmin.ferreiro@despegar.com>
2018-06-15 21:12:56 -0300

    vga

--- decls.c
+++ decls.c
@@ -1,11 +1,11 @@
 #include "decls.h"
 
-static volatile char *const VGABUF = (volatile char *) 0xb8000;
+volatile char *const VGABUF = (volatile char *) 0xb8000;
 
 void vga_write(const char *s, int8_t linea, uint8_t color){
-	*(VGABUF + 120) += 88;
-
+	//*(VGABUF + 120) += 88;
     volatile char *buf = VGABUF;
+    buf+=2*80*linea;
 
     while( *s != 0 ){
         *buf++ = *s++;
